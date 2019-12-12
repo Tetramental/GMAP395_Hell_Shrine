22e76cc3605f2450846ea731d710da1cf30965b1
Lange Eo <tetraquadra@gmail.com>
2019-12-12 13:55:05 -0500

    Created script for enabling lights after trigger has been detected

---
+++ Assets/_Hell Shrine/Scripts/EnableLight.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableLight : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject object1;
+    [SerializeField]
+    private GameObject object2;
+    [SerializeField]
+    private GameObject object3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        object1.SetActive(false);
+        object2.SetActive(false);
+        object3.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        object1.SetActive(true);
+        object2.SetActive(true);
+        object3.SetActive(true);
+    }
+}
