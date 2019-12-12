41cd01582b410e5d10ccafae04bdd49125d4da88
Lange Eo <tetraquadra@gmail.com>
2019-12-12 13:55:21 -0500

    Created script for fading in of scene start

---
+++ Assets/_Hell Shrine/Scripts/FadeIn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeIn : MonoBehaviour
+{
+    Renderer rend;
+    public Color alpha;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = this.GetComponent<Renderer>();
+        alpha = rend.material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (alpha.a > 0f)
+        {
+            alpha.a -= 0.1f * Time.deltaTime;
+            rend.material.SetColor("_BaseColor", alpha);
+        }
+        else
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
