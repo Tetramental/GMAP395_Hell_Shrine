0815f595c532c7c354f6d510d1010d5f60ccdb25
Lange Eo <tetraquadra@gmail.com>
2019-12-12 13:54:37 -0500

    Created camera disable script for switching between cameras

---
+++ Assets/_Hell Shrine/Scripts/DisableCameraScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableCameraScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject playerMovement;
+    [SerializeField]
+    private GameObject flyByCamMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerMovement.GetComponent<PlayerMovement>().enabled = true;
+        flyByCamMovement.GetComponent<SimpleCameraController>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            playerMovement.GetComponent<PlayerMovement>().enabled = true;
+            flyByCamMovement.GetComponent<SimpleCameraController>().enabled = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            playerMovement.GetComponent<PlayerMovement>().enabled = false;
+            flyByCamMovement.GetComponent<SimpleCameraController>().enabled = true;
+        }
+    }
+}
