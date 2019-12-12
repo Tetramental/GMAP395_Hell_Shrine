2ba9d57c57aa2d939379f1f994e1a8f48f33f819
Lange Eo <tetraquadra@gmail.com>
2019-12-12 13:55:44 -0500

    Created script for tracking mouse movement and looking around

---
+++ Assets/_Hell Shrine/Scripts/MouseLook.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField]
+    private float mouseSensitivity = 100f;
+
+    [SerializeField]
+    private Transform playerBody;
+
+    private float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // stop application when ESC is pressed
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            #endif
+        }
+
+        // unlock visibility and usage of mouse cursor in-play
+        if (Input.GetKey(KeyCode.BackQuote))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+        // update mouse x and y position values over time
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // update value of x rotation of camera and force it to stay between -60 degrees and 60 degrees in the x axis
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -60f, 60f);
+
+        // update camera x rotation
+        // update player y rotation according to mouse left and right swipe
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
