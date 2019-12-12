e9bf7c7c95c3350e8182af5028a1b649a01284e1
Lange Eo <tetraquadra@gmail.com>
2019-12-12 13:55:53 -0500

    Created script for player movement

---
+++ Assets/_Hell Shrine/Scripts/PlayerMovement.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private CharacterController controller;
+
+    [SerializeField]
+    private float walkSpeed = 1.5f;
+    private float walkSpeedAddedValue;
+
+    [SerializeField]
+    private float gravity = -9.81f;
+    [SerializeField]
+    private float jumpHeight = 3f;
+
+    [SerializeField]
+    private Transform groundCheck;
+    [SerializeField]
+    private float groundDistance = 0.4f;
+    [SerializeField]
+    private LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && (walkSpeed > 0f))
+        {
+            walkSpeedAddedValue = (walkSpeed * 5f) - walkSpeed;
+            walkSpeed += walkSpeedAddedValue;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift) & (walkSpeed > 0f))
+        {
+            walkSpeed -= walkSpeedAddedValue;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = (transform.right * x + transform.forward * z).normalized;
+
+        controller.Move(move * walkSpeed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Last Trigger")
+        {
+            walkSpeed = 5f;
+            other.gameObject.SetActive(false);
+        }
+    }
+}
