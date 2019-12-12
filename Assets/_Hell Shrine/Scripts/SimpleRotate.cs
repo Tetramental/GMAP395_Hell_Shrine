bfca1c7d94cf247bb846f387a2d63b226a671f35
Lange Eo <tetraquadra@gmail.com>
2019-12-12 13:57:02 -0500

    Created script to rotate lotus flowers

---
+++ Assets/_Hell Shrine/Scripts/SimpleRotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRotate : MonoBehaviour
+{
+    [SerializeField]
+    private float rotateSpeed = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.localScale.x >= 1f)
+        {
+            transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
+        }
+        else if (transform.localScale.x <= -1f)
+        {
+            transform.Rotate(0, 0, -(rotateSpeed * Time.deltaTime));
+        }
+    }
+}
